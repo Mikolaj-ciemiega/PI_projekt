ea8edb596fdeeb7c72d4dede2258e859ab6a47ed
Kamil-Klecz <115651508+Kamil-Klecz@users.noreply.github.com>
2022-11-09 00:19:19 +0200

    Task  #1  dodaj plik AES_implementacja
    - Dodano plik AES_implementacja.cpp zawierający instrukcje szyfrowania tekstu kodem AES

---
+++ AES_implementacja.cpp
@@ -0,0 +1,44 @@
+﻿// Tworzenie_Kluczy.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+
+
+
+
+/*
+strona pokazująca implementację kodu szyfrującego AES do c++
+ https://programmer.group/c-implementation-of-aes-encryption-algorithms.html
+
+
+ strona tłumaczaca działanie AES
+ https://cybernews.com/resources/what-is-aes-encryption/
+
+
+
+ 1. dzielenie tekstu na bloki po 16 bajtów (16 liter)
+ 2. Utworzenie klucza (np. makowiec)
+ 3. wykonanie kilku operacji matematycznych dla każdego z bajtów klucza, rozszerzając go
+ 4. dodanie (binarne) rozszerzonego klucza do każdego z bloków (1 poziom szyfrowania)
+ 5. odjecia od każdego bloku wartości z predefiniowanej tabeli (Rijndael S-box)
+ 6. tasowanie wierszy i kolumn w kazdym bloku
+ 7. ponowne dodanie rozszerzonego glucza do bloków
+ 8. całość powtarzamy 10 (lub wiecej) razy 
+
+ */
+
+
+
+
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Test\n";
+
+}
+
+
