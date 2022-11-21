156900c07b9ccef193869c4962505934787106a0
Kamil-Klecz <kk305373@student.polsl.pl>
2022-11-21 21:41:34 +0100

    Updated file AES_implemntacja

--- AES_implementacja.cpp
+++ AES_implementacja.cpp
@@ -1,44 +1,172 @@
-﻿// Tworzenie_Kluczy.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <string.h>
 
-
-
-
-
-/*
-strona pokazująca implementację kodu szyfrującego AES do c++
- https://programmer.group/c-implementation-of-aes-encryption-algorithms.html
-
-
- strona tłumaczaca działanie AES
- https://cybernews.com/resources/what-is-aes-encryption/
-
-
-
- 1. dzielenie tekstu na bloki po 16 bajtów (16 liter)
- 2. Utworzenie klucza (np. makowiec)
- 3. wykonanie kilku operacji matematycznych dla każdego z bajtów klucza, rozszerzając go
- 4. dodanie (binarne) rozszerzonego klucza do każdego z bloków (1 poziom szyfrowania)
- 5. odjecia od każdego bloku wartości z predefiniowanej tabeli (Rijndael S-box)
- 6. tasowanie wierszy i kolumn w kazdym bloku
- 7. ponowne dodanie rozszerzonego glucza do bloków
- 8. całość powtarzamy 10 (lub wiecej) razy 
-
- */
-
-
-
-
-
+#include <iomanip>
 using namespace std;
-
 
 int main()
 {
-    cout << "Test\n";
+    setlocale(LC_ALL, "");
 
+    //TEKST
+
+    char tekst[65] = { "\0" };
+    cout << "podaj tekst\n";
+    fgets(tekst, 64, stdin);
+
+    char tab[4][4][4] = {};
+
+
+
+    int licznik2 = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                tab[i][j][k] = tekst[licznik2];
+
+                licznik2++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+
+                cout << tab[i][j][k];
+
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+
+    //klucz
+    char klucz[17] = {};
+    cout << "wpisz klucz" << endl;
+    fgets(klucz, 17, stdin);
+
+
+
+
+    char tabK[4][4][4] = { { {'e', 'x', 'p', 'a'}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}},
+                           { {1, 2, 3, 4}, {'n', 'd', ' ', '1'}, {33, 33, 33, 33}, {33, 33, 33, 33}},
+                           { {33, 33, 33, 33}, {33, 33, 33, 33}, {'6', '-', 'b', 'y'}, {1, 2, 3, 4}},
+                           { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {'t', 'e', ' ', 'k'}} };
+
+    int licznik = 0;
+
+
+
+
+    //aaaabbbbccccdddd
+
+
+    for (int i = 0; i < 4; i++) {
+
+        tabK[0][1][i] = klucz[licznik];
+        tabK[2][3][i] = klucz[licznik];
+        licznik++;
+    }
+
+    for (int i = 0; i < 4; i++) {
+
+        tabK[0][2][i] = klucz[licznik];
+        tabK[3][0][i] = klucz[licznik];
+        licznik++;
+    }
+
+    for (int i = 0; i < 4; i++) {
+
+        tabK[0][3][i] = klucz[licznik];
+        tabK[3][1][i] = klucz[licznik];
+        licznik++;
+    }
+
+    for (int i = 0; i < 4; i++) {
+
+        tabK[1][0][i] = klucz[licznik];
+        tabK[3][2][i] = klucz[licznik];
+        licznik++;
+    }
+
+
+
+
+
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+
+
+                cout << tabK[i][j][k];
+
+
+                licznik++;
+            }
+            cout << "|";
+        }
+        cout << endl;
+    }
+
+
+    cout << endl << "część z tekstem:" << endl;
+
+
+
+
+
+
+
+    /*
+    string Tekst = "abcd";
+    string Klucz = "23sd";
+    string TekstE = "0000";
+    string TekstD = "0000";
+
+    for (int i = 0; i < 4; i++) {
+
+        //TekstE
+        TekstE[i] = ((Tekst[i] - '0') ^ (Klucz[i] - '0')) + '0';
+
+
+    }
+    //odwracamy - dekrypcja
+
+    for (int i = 0; i < 4; i++) {
+
+
+        TekstD[i] = ((TekstE[i] - '0') ^ (Klucz[i] - '0')) + '0';
+
+
+    }
+
+
+    cout << "a= " << a << endl;
+    cout << "b= " << b << endl;
+    cout << "Tekst= " << Tekst << endl;
+    cout << "Klucz= " << Klucz << endl;
+    cout << "Tekst po enkrypcji XOR-em= " << TekstE << endl;
+    cout << "Tekst po de-enkrypcji XOR-em= " << TekstD << endl;
+    */
+    return 0;
 }
 
 
