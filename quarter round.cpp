845e7f1efc8ed32c3104192a191c3ff0bdc20e81
Mikolaj-ciemiega <mc305352@student.polsl.pl>
2023-01-08 18:01:29 +0100

    completed quarterround and littleendian functions
    key table is correctly shuffled and prepered for XOR function

--- quarter round.cpp
+++ quarter round.cpp
@@ -1,17 +1,20 @@
 ﻿#include <iostream>
-#include <cstdio>
 #include <cmath>
 #include <cstring>
-#include <stddef.h>
 
 using namespace std;
 
 void quarterround(char* y0, char* y1, char* y2, char* y3)
 {
-    *y1 = *y1 ^ ((*y0 + *y3) << 7);
-    *y2 = *y2 ^ ((*y1 + *y0) << 9);
-    *y3 = *y3 ^ ((*y2 + *y1) << 13);
-    *y0 = *y0 ^ ((*y3 + *y2) << 18);
+    char x0 = *y0 + *y3, x1 = 0, x2 = 0, x3 = 0;
+
+    *y1 = *y1 ^ ((x0) <<= 7);
+    x1 = *y1 + *y0;
+    *y2 = *y2 ^ ((x1) <<= 9);
+    x2 = *y2 + *y1;
+    *y3 = *y3 ^ ((x2) <<= 13);
+    x3 = *y3 + *y2;
+    *y0 = *y0 ^ ((x3) <<= 18);
 }
 
 void rowround(char* y)
@@ -23,11 +26,6 @@ void rowround(char* y)
         quarterround(y + i + 40, y + i + 44, y + i + 32, y + i + 36);
         quarterround(y + i + 60, y + i + 48, y + i + 52, y + i + 56);
     }
-    /*
-        quarterround(&y[0], &y[1], &y[2], &y[3]);
-        quarterround(&y[5], &y[6], &y[7], &y[4]);
-        quarterround(&y[10], &y[11], &y[8], &y[9]);
-        quarterround(&y[15], &y[12], &y[13], &y[14]);*/
 }
 
 void columnround(char* x)
@@ -39,11 +37,6 @@ void columnround(char* x)
         quarterround(x + i + 40, x + i + 56, x + i + 8, x + i + 24);
         quarterround(x + i + 60, x + i + 12, x + i + 28, x + i + 44);
     }
-    /*
-        quarterround(&x[0], &x[4], &x[8], &x[12]);
-        quarterround(&x[5], &x[9], &x[13], &x[1]);
-        quarterround(&x[10], &x[14], &x[2], &x[6]);
-        quarterround(&x[15], &x[3], &x[7], &x[11]);*/
 }
 
 void doubleround(char* x)
@@ -52,12 +45,12 @@ void doubleround(char* x)
     rowround(x);
 }
 
-char littleendian(char input, int i)
+char littleendian(char input, int j)
 {
-    return (input << (i * 8));
+    return (input <<= j);
 }
 
-void hash_salsa(char* wsk_tabK, char* wsk_output)
+void hash_salsa(char* wsk_imput, char* wsk_output)
 {
     char words[16][4], coppy_words[16][4];
     int licznik = 0;
@@ -66,12 +59,13 @@ void hash_salsa(char* wsk_tabK, char* wsk_output)
     {
         for (int j = 0; j < 4; j++)
         {
-            words[i][j] = *wsk_tabK;
-            coppy_words[i][j] = *wsk_tabK;
-            wsk_tabK++;
-        }
-    }
+            words[i][j] = *wsk_imput;
+            coppy_words[i][j] = *wsk_imput;
+            wsk_imput++;
 
+        }
+        cout << endl;
+    };
     for (int i = 0; i < 16; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -79,6 +73,7 @@ void hash_salsa(char* wsk_tabK, char* wsk_output)
             words[i][j] = littleendian(coppy_words[i][j], j);
         }
     }
+
     char* wsk_words = &words[0][0];
 
     doubleround(wsk_words);
@@ -91,45 +86,39 @@ void hash_salsa(char* wsk_tabK, char* wsk_output)
         }
     }
 
+    cout << "output" << endl;
+
     for (int i = 0; i < 16; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            *wsk_output = words[i][j];
+            if ((int)words[i][j] < 0)
+            {
+                words[i][j] = words[i][j] * -1;
+            }
+
+            *wsk_output = words[i][j] + 32;
+
             wsk_output++;
         }
+        cout << endl;
     }
 
 }
 
 int main()
 {
-    char tabK[4][4][4] = { { {'e', 'x', 'p', 'a'}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}},
+    char imput[4][4][4] = { { {'e', 'x', 'p', 'a'}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}},
                            { {1, 2, 3, 4}, {'n', 'd', ' ', '3'}, {33, 33, 33, 33}, {33, 33, 33, 33}},
                            { {33, 33, 33, 33}, {33, 33, 33, 33}, {'2', '-', 'b', 'y'}, {1, 2, 3, 4}},
                            { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {'t', 'e', ' ', 'k'}} };
-    char* wsk_tabK;
-    wsk_tabK = &tabK[0][0][0];
-
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            for (int k = 0; k < 4; k++)
-            {
-
-                cout << tabK[i][j][k];
-
-            }
-            cout << "|";
-        }
-        cout << endl;
-    }
 
     char output[4][4][4] = {};
-    char* wsk_output = &output[0][0][0];
 
-    hash_salsa(wsk_tabK, wsk_output);
+
+    char* wsk_output = &output[0][0][0], *wsk_imput=&imput[0][0][0];
+
+    hash_salsa(wsk_imput, wsk_output);
 
     for (int i = 0; i < 4; i++)
     {
@@ -137,15 +126,11 @@ int main()
         {
             for (int k = 0; k < 4; k++)
             {
-
                 cout << output[i][j][k];
-
             }
             cout << "|";
         }
         cout << endl;
     }
 
-
 }
-
